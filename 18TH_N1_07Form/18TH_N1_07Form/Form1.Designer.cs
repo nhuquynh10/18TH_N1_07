3f49b3b51af9d8ac8be42cfcda459632890a6cc0
nhựt nguyễn minh <nhut09112000@gmail.com>
2022-02-28 11:08:55 +0700

    comit

--- 18TH_N1_07Form/18TH_N1_07Form/Form1.Designer.cs
+++ 18TH_N1_07Form/18TH_N1_07Form/Form1.Designer.cs
@@ -39,20 +39,17 @@ namespace _18TH_N1_07Form
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(134, 66);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(179, 81);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(122, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên đăng nhập999:";
->>>>>>> fe62c43b1d31d792ea703c3cf24f60f53bac3432
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 112);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(189, 138);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 16);
             this.label2.TabIndex = 1;
@@ -61,29 +58,28 @@ namespace _18TH_N1_07Form
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(291, 66);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUsername.Location = new System.Drawing.Point(388, 81);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(182, 20);
+            this.txtUsername.Size = new System.Drawing.Size(241, 22);
             this.txtUsername.TabIndex = 2;
             this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(211, 112);
-            this.txtPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPass.Location = new System.Drawing.Point(281, 138);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(182, 20);
+            this.txtPass.Size = new System.Drawing.Size(241, 22);
             this.txtPass.TabIndex = 3;
             this.txtPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // Login
             // 
-            this.Login.Location = new System.Drawing.Point(169, 157);
-            this.Login.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Login.Location = new System.Drawing.Point(225, 193);
+            this.Login.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(56, 19);
+            this.Login.Size = new System.Drawing.Size(136, 23);
             this.Login.TabIndex = 4;
             this.Login.Text = "Login 111111";
             this.Login.UseVisualStyleBackColor = true;
@@ -91,15 +87,15 @@ namespace _18TH_N1_07Form
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.Login);
             this.Controls.Add(this.txtPass);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "btnLogin";
             this.Load += new System.EventHandler(this.Form1_Load);
